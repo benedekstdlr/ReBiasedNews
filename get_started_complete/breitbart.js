e8e4598496d03b4f6426870544f0a240d2e3d7a1
kacsa123 <35614487+kacsa123@users.noreply.github.com>
2020-01-18 15:24:04 +0000

    Update breitbart.js
    Change breitbart politics articles to have REEEEEEEEEEEEEE after the first paragraph

--- get_started_complete/breitbart.js
+++ get_started_complete/breitbart.js
@@ -1,5 +1,24 @@
 'use strict';
-console.log("BREITBART");
-chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '"; console.log("BREITBART");'});
+window.onload = function() {
+    console.log("BREITBART");
+    //window.alert("Breitbart!!!");
+
+    var title = document.getElementsByTagName("h1")[0];
+    title.innerHTML = "Hello World!";
+
+    var desc = document.getElementsByClassName("subheading")[0];
+    //var hasDesc = false;
+    console.log(desc);
+    if (desc) {
+        //hasDesc = true;
+        console.log("Description exists");
+    } else {
+        var desc = document.getElementsByClassName("entry-content")[0].childNodes[1];
+    }
+    var descText = desc.innerHTML;
+    console.log(descText);
+    console.log(desc);
+    var generatedText = "REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE";
+    document.getElementsByClassName("entry-content")[0].innerHTML = '<p class="subheading">'+descText+"<\p>"+"<p>"+generatedText+"<\p>";
+    
+};
