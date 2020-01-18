dbb17a1535685778cc24f037dfc1ef6312bedd3c
Peti <pmernyei@gmail.com>
2020-01-18 23:12:19 +0000

    Server serving stuff

--- server/app.py
+++ server/app.py
@@ -1,29 +1,23 @@
-from http.server import SimpleHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler
 from socketserver import TCPServer
-DUMMY_RESPONSE = """Content-type: text/html
+import gpt_2_simple as gpt2
 
-<html>
-<head>
-<title>Python Test</title>
-</head>
+CP_DIR = '../../models/breitbart/checkpoint'
 
-<body>
-Test page...success.
-</body>
-</html>
-"""
+sess = gpt2.start_tf_sess()
+gpt2.load_gpt2(sess, checkpoint_dir=CP_DIR)
 
-class MyHandler(SimpleHTTPRequestHandler):
-    def __init__(self, request, client_address, server):
-        SimpleHTTPRequestHandler.__init__(self, request, client_address, server)
-
-
-    def do_GET(self):
+class MyHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        print(rfile.read())
+        #text = gpt2.generate(sess, checkpoint_dir=CP_DIR, return_as_list=True, prefix=rfile.read())[0]
+        text = ''
+        text = str.encode(text)
         self.send_response(200)
         self.send_header("Content-type", "text/html")
-        self.send_header("Content-length", len(DUMMY_RESPONSE))
+        self.send_header("Content-length", len(text))
         self.end_headers()
-        self.wfile.write(str.encode(DUMMY_RESPONSE))
+        #self.wfile.write(text)
 
 PORT = 80
 
