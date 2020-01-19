6c732abdb7c10625b532c79090553d55196c2346
kacsa123 <35614487+kacsa123@users.noreply.github.com>
2020-01-19 08:39:19 +0000

    Update breitbart.js

--- get_started_complete/breitbart.js
+++ get_started_complete/breitbart.js
@@ -1,35 +1,39 @@
 'use strict';
-var titleText = document.getElementsByTagName("h1")[0].textContent;
+    var titleText = document.getElementsByTagName("h1")[0].textContent;
 
-var desc = document.getElementsByClassName("subheading")[0];
-//var hasDesc = false;
-if (desc) {
-    //hasDesc = true;
-    console.log("Description exists");
-} else {
-    var desc = document.getElementsByClassName("entry-content")[0].childNodes[1];
-}
-var descText = desc.textContent;
-console.log("DESC: "+descText);
-console.log("DESC: "+desc);
-
-var request = new XMLHttpRequest();
-
-request.open('POST', 'https://rebiasednews.ddns.net', true);
-request.onerror = function() {
-    window.alert("Sorry, this article is not ready yet, try again in a few minutes");
-}
-request.onload = function() {
-    if(this.response === undefined || this.response.length == 0) {
-        // TODO: nothing found
-        console.log(this.response);
-        console.log("Nothing found");
+    var desc = document.getElementsByClassName("subheading")[0];
+    //var hasDesc = false;
+    if (desc) {
+        //hasDesc = true;
+        console.log("Description exists");
     } else {
-        var resp = this.response;
-        console.log(resp);
-        document.getElementsByClassName("entry-content")[0].innerHTML = "<p>"+resp+"<\p>";
+        var desc = document.getElementsByClassName("entry-content")[0].childNodes[1];
     }
-};
-request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-var data = JSON.stringify({"title": titleText, "body": descText});
-request.send(data);
+    var descText = desc.textContent;
+    console.log("DESC: "+descText);
+    console.log("DESC: "+desc);
+    
+    var request = new XMLHttpRequest();
+
+    request.open('POST', 'https://rebiasednews.ddns.net', true);
+    request.onerror = function() {
+        window.alert("Sorry, this article is not ready yet, try again in a few minutes");
+    }
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            if(this.response === undefined || this.response.length == 0) {
+                // TODO: nothing found
+                console.log(this.response);
+                console.log("Nothing found");
+            } else {
+                var resp = this.response;
+                console.log(resp);
+                document.getElementsByClassName("entry-content")[0].innerHTML = "<p>"+resp+"<\p>";
+            }
+        } else if (this.readyState == 4 && this.status == 500) {
+            window.alert("Sorry, this article is not ready yet, try again in a few minutes");
+        }
+    };
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    var data = JSON.stringify({"title": titleText, "body": descText});
+    request.send(data);
