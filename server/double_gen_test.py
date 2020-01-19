bdb06733e18427d2629bb3c4d3e48089db4203c0
Peti <pmernyei@gmail.com>
2020-01-19 03:04:28 +0000

    More test stuff

--- server/double_gen_test.py
+++ server/double_gen_test.py
@@ -7,9 +7,10 @@ import ssl
 CP_DIR_1 = '../../models/breitbart/checkpoint'
 CP_DIR_2 = '../../models/cnn/checkpoint'
 
-sess1 = gpt2.start_tf_sess()
-sess2 = gpt2.start_tf_sess()
+sess = gpt2.start_tf_sess(threads=7)
 gpt2.load_gpt2(sess1, checkpoint_dir=CP_DIR_1)
-gpt2.load_gpt2(sess2, checkpoint_dir=CP_DIR_2)
-gpt2.generate(sess1)
-gpt2.generate(sess2)
+gpt2.generate(sess)
+
+sess = gpt2.reset_session(sess, threads=7)
+gpt2.load_gpt2(sess, checkpoint_dir=CP_DIR_2)
+gpt2.generate(sess)
