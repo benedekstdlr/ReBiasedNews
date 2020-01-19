093c7d68dfe3f0f226f7aeb4218ca50dc8c76b63
Peti <pmernyei@gmail.com>
2020-01-19 04:46:37 +0000

    producer consumer magick

--- server/app.py
+++ server/app.py
@@ -5,6 +5,9 @@ import json
 import ssl
 import hashlib
 import random
+import threading
+import Queue
+import time
 
 BB_DIR = '../../models/breitbart/checkpoint'
 CNN_DIR = '../../models/cnn/checkpoint'
@@ -12,6 +15,7 @@ CNN_DIR = '../../models/cnn/checkpoint'
 last_model = None
 sess = gpt2.start_tf_sess()
 
+q = Queue.Queue(10000)
 
 def model_dir(origin):
     if 'breitbart' in origin:
@@ -20,7 +24,14 @@ def model_dir(origin):
         return BB_DIR
 
 
-class MyHandler(BaseHTTPRequestHandler):
+class GeneratorThread(threading.Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs=None, verbose=None):
+        super(GeneratorThread,self).__init__()
+        self.target = target
+        self.name = name
+        return
+
     def generate_new(self, origin, vals):
         return ('stuff ' + str(random.randrange(10000))).encode('utf-8')
         dir = source(origin)
@@ -33,6 +44,24 @@ class MyHandler(BaseHTTPRequestHandler):
         text = str.encode(text)
         return text
 
+    def run(self):
+        while True:
+            if not q.empty():
+                time.sleep(30)
+                (hash, origin, vals) = q.get()
+                print('Getting \"' + str(vals['title']) + '\" with hash ' + hash +
+                              + ' : ' + str(q.qsize()) + ' items in queue')
+                text = self.generate_new(origin, vals)
+                f = open('../../out/'+hash, 'wb')
+                f.write(text)
+                f.close()
+            time.sleep(1)
+        return
+
+
+
+
+class MyHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         print(self.headers)
@@ -41,18 +70,16 @@ class MyHandler(BaseHTTPRequestHandler):
         vals = json.loads(body)
         referer = self.headers.get('referer')
         origin = self.headers.get('origin')
-         hash = hashlib.md5(referer.encode('utf-8')).hexdigest()
-         try:
-             print('Accessing from cache')
-             f = open('../../out/'+hash, 'rb')
-             text = f.read()
-             f.close()
-         except IOError:
-             print('Generating new')
-             text = self.generate_new(origin, vals)
-             f = open('../../out/'+hash, 'wb')
-             f.write(text)
-             f.close()
+        hash = hashlib.md5(referer.encode('utf-8')).hexdigest()
+        try:
+            print('Accessing from cache')
+            f = open('../../out/'+hash, 'rb')
+            text = f.read()
+            f.close()
+        except IOError:
+            print('Generating new')
+            q.put((hash, origin, vals))
+            text = b'... And then what? Check back later to find out!'
 
 
         self.send_response(200)
@@ -67,12 +94,12 @@ class MyHandler(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
-
 PORT = 443
-
 httpd = TCPServer(("", PORT), MyHandler)
 
 print("serving at port", PORT)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='/etc/letsencrypt/live/rebiasednews.ddns.net/fullchain.pem',
         keyfile='/etc/letsencrypt/live/rebiasednews.ddns.net/privkey.pem', server_side=True)
 httpd.serve_forever()
+
+GeneratorThread(name='generator-thread').start()
