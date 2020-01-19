50535f8b561c660cbe000dfa8c95cc202d6cce2a
kacsa123 <35614487+kacsa123@users.noreply.github.com>
2020-01-19 03:18:45 +0000

    Update breitbart.js

--- get_started_complete/breitbart.js
+++ get_started_complete/breitbart.js
@@ -1,19 +1,32 @@
 'use strict';
-    var title = document.getElementsByTagName("h1")[0];
-    title.innerHTML = "Hello World!";
+var titleText = document.getElementsByTagName("h1")[0].textContent;
 
-    var desc = document.getElementsByClassName("subheading")[0];
-    //var hasDesc = false;
-    console.log(desc);
-    if (desc) {
-        //hasDesc = true;
-        console.log("Description exists");
+var desc = document.getElementsByClassName("subheading")[0];
+//var hasDesc = false;
+if (desc) {
+    //hasDesc = true;
+    console.log("Description exists");
+} else {
+    var desc = document.getElementsByClassName("entry-content")[0].childNodes[1];
+}
+var descText = desc.textContent;
+console.log("DESC: "+descText);
+console.log("DESC: "+desc);
+
+var request = new XMLHttpRequest();
+
+request.open('POST', 'https://rebiasednews.ddns.net', true);
+request.onload = function() {
+    if(this.response === undefined || this.response.length == 0) {
+        // TODO: nothing found
+        console.log(this.response);
+        console.log("Nothing found");
     } else {
-        var desc = document.getElementsByClassName("entry-content")[0].childNodes[1];
+        var resp = this.response;
+        console.log(resp);
+        document.getElementsByClassName("entry-content")[0].innerHTML = '<p class="subheading">'+descText+"<\p>"+"<p>"+resp+"<\p>";
     }
-    var descText = desc.innerHTML;
-    console.log(descText);
-    console.log(desc);
-    var generatedText = "REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE";
-    document.getElementsByClassName("entry-content")[0].innerHTML = '<p class="subheading">'+descText+"<\p>"+"<p>"+generatedText+"<\p>";
-    
+};
+request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+var data = JSON.stringify({"title": titleText, "body": descText});
+request.send(data);
